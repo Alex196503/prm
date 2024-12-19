ab61470eaaee63070de5c6861b8ef849f70cb246
Alexandru Moldovan <alexmoldovan2009@gmail.com>
2024-12-19 22:14:31 +0200

    doar autentificarea si autorizarea au ramas

---
+++ Models/ViewModels/MemberIndexData.cs
@@ -0,0 +1,9 @@
+﻿using ProiectMediiBun.Models;
+namespace ProiectMediiBun.Models.ViewModels
+{
+    public class MemberIndexData
+    {
+        public IEnumerable<Member> Members { get; set; }
+        public IEnumerable <Membership> Memberships { get; set; }
+    }
+}
--- Pages/Members/Index.cshtml.cs
+++ Pages/Members/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using ProiectMediiBun.Data;
 using ProiectMediiBun.Models;
 using ProiectMediiBun.Models;
+using ProiectMediiBun.Models.ViewModels;
 
 namespace Proiect_Medii.Pages.Members
 {
@@ -21,13 +22,55 @@ namespace Proiect_Medii.Pages.Members
         }
 
         public IList<Member> Member { get; set; } = default!;
+        public MemberIndexData MemberData { get; set; }
+        public int MemberID { get; set; }
 
-        public async Task OnGetAsync()
+        public int MembershipID { get; set; }
+        public string FirstNameSort { get; set; }
+        public string TrainerNameSort { get; set; }
+        public string CurrentFilter { get; set; }
+
+
+        public async Task OnGetAsync(int? id,int?membershipID,string SortOrder,string searchString)
         {
-            Member = await _context.Member
+            MemberData = new MemberIndexData();
+            FirstNameSort = String.IsNullOrEmpty(SortOrder) ? "firstname_desc" : "";
+            TrainerNameSort = SortOrder == "trainer" ? "trainer_desc" : "trainer";
+            CurrentFilter = searchString;
+            MemberData.Members = await _context.Member
                 .Include(m => m.Membership)
                 .Include(m => m.Trainer)
+                .OrderBy(m=>m.LastName)
                 .ToListAsync();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                MemberData.Members = MemberData.Members.Where(s => s.Trainer.Name.Contains(searchString)
+                || s.FirstName.Contains(searchString));
+            }
+            if(id!=null)
+            {
+                MemberID = id.Value;
+                var membruselectat = MemberData.Members
+                    .Where(m => m.ID == id.Value).Single();
+                MemberData.Memberships=membruselectat.Membership!=null
+                    ?new List<Membership> { membruselectat.Membership}
+                    : new List<Membership>();
+            }
+        switch(SortOrder)
+            {
+                case "title_desc":
+                    MemberData.Members = MemberData.Members.OrderByDescending(s => s.FirstName);
+                    break;
+                case "trainer_desc":
+                    MemberData.Members = MemberData.Members.OrderByDescending(s => s.Trainer.Name);
+                    break;
+                case "trainer":
+                    MemberData.Members = MemberData.Members.OrderBy(s => s.Trainer.Name);
+                    break;
+                default:
+                    MemberData.Members = MemberData.Members.OrderBy(s => s.FirstName);
+                    break;
+            }
         }
     }
 }
